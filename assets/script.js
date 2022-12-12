d85f6b609d67252140df8f8667fb23dee9d82317
Xavionstar <lincpough@gmail.com>
2022-12-11 21:21:34 -0800

    Added final css changes and comments on the JS

--- assets/script.js
+++ assets/script.js
@@ -10,34 +10,36 @@ var $searchHistory = $("#searchHistory");
 var $searchHistoryButton = $(".searchHistoryButton")
 var $weatherIcon = $("#weatherIcon")
 
+//This function runs the search history at the start 
 updateSearchButtons()
 
+//This fucntion calls up the weather in the city according to the weather API
 function fetchCurrentWeatherData(city) {
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
     fetch(queryURL, {
         method: "GET"
-
     })
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            $searchedCityName.text(data.name);
+            $searchedCityName.text(data.name + " " + (new Date()).toLocaleDateString());
+            console.log(data)
             $searchedCityTemp.text(data.main.temp);
             $searchedCityWind.text(data.wind.speed);
             $searchedCityHumidity.text(data.main.humidity);
-            $weatherIcon.html(`<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`)
+            $weatherIcon.html(`<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" width="40" height="40">`)
         })
         .catch(function (error){
             alert("Error, not a city!")
         });
 }
+//This function calls up the weather for the next 5 days in said city
 function fetchForecastWeatherData(city) {
     $forecastContainer.html("");
     var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
     fetch(queryURL, {
         method: "GET"
-
     })
         .then(function (response) {
             return response.json();
@@ -52,6 +54,7 @@ function fetchForecastWeatherData(city) {
             
         });
 }
+//This function takes the weather api forecast data and turns it to HTML 
 function getSingleDayWeatherHtml(singleDayWeatherData) {
     var singleDayWeatherHtml = $("<li/>");
     var singleDayWeatherDate = $(`<h3> ${singleDayWeatherData.dt_txt} <h3/>`);
@@ -66,7 +69,7 @@ function getSingleDayWeatherHtml(singleDayWeatherData) {
     singleDayWeatherHtml.append(singleDayForecast);
     return singleDayWeatherHtml;
 }
-
+//this function sets a searched city on the local storage
 function addSearchedCity(city) {
     var searchHistory = getSearchHistory()
     searchHistory.unshift(city)
@@ -75,6 +78,7 @@ function addSearchedCity(city) {
     }
     localStorage.setItem("searchedCities", JSON.stringify(searchHistory));
 }
+//This function creates a button for each city searched
 function updateSearchButtons() {
     $searchHistory.html("")
     var searchHistory = getSearchHistory()
@@ -84,12 +88,12 @@ function updateSearchButtons() {
 
     }
 }
-
+//this function pulls info from local storage to name the buttons
 function getSearchHistory() {
     var searchHistory = JSON.parse(localStorage.getItem("searchedCities") || '[]');
     return searchHistory
 }
-
+//This button will allow you to repeat searches from the search history
 $(document).on("click", ".searchHistoryButton", function (e) {
     var city =$(e.currentTarget).text();
     $cityInput.val(city)
@@ -98,7 +102,7 @@ $(document).on("click", ".searchHistoryButton", function (e) {
     addSearchedCity(city);
     updateSearchButtons();
 })
-
+//This button will get the weather for today, for the next 5 days, add it to search history and create a button that is useable to repeat a search
 $searchButton.on("click", function () {
     var city = $cityInput.val();
     fetchCurrentWeatherData(city);
